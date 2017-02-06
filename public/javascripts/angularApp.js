ec8fa07c7bd865e97539951dbd786078b625f21e
stas7t <stas7t@gmail.com>
2017-02-06 13:48:41 +0200

    currentUser id

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -72,8 +72,7 @@ app.factory('auth', ['$http', '$window', function($http, $window){
       if(auth.isLoggedIn()){
         var token = auth.getToken();
         var payload = JSON.parse($window.atob(token.split('.')[1]));
-        console.log('payload');
-        console.log(payload);
+
         return payload;
       }
     };
