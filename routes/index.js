fb06b7061fe7daac2bd1ed1c6227c6123649e658
stas7t <stas7t@gmail.com>
2017-02-11 19:35:14 +0200

    33

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,14 @@ var express = require('express');
 var router = express.Router();
 
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL
+var connectionString = process.env.DATABASE_URL || 'postgres://scsksajxkfacju:63f674e8151fe9a9948849388151831a418571b222e5af36a5f7c8fd7d48b8a7@ec2-23-23-186-157.compute-1.amazonaws.com:5432/d2vogc5ech97g'
 
 var mongoose = require('mongoose');
 var passport = require('passport');
 var jwt = require('express-jwt');
 var User = mongoose.model('User');
 
-var auth = jwt({secret: process.env.SECRET, userProperty: 'payload'});
+var auth = jwt({secret: process.env.SECRET || 'rubygarage2017rubygarage', userProperty: 'payload'});
 
 /* GET home page. */
 /*router.get('/', function(req, res) {
