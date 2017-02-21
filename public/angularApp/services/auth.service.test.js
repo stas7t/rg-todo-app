27ea793f585b50bff2c3fb7e7a9e48a074088d2b
stas7t <stas7t@gmail.com>
2017-02-22 00:07:27 +0200

    auth service tests

--- public/angularApp/services/auth.service.test.js
+++ public/angularApp/services/auth.service.test.js
@@ -1,5 +1,48 @@
-describe('Users factory', function() {
-    it('has a dummy spec to test 2 + 2', function() {
-        expect(2+2).toEqual(4);
+describe('Auth factory', function() {
+    var auth;
+    var $httpBackend;
+
+    var user = {  username:'john dou',
+                    password:'123qwe' };
+
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OGFjYWM5MDU0YzZjMjAwMDQ1OTJhZTciLCJ1c2VybmFtZSI6ImpvaG4gZG91IiwiZXhwIjoxNDkwMzAzMzc2LCJpYXQiOjE0ODc3MTEzNzZ9.g5iX088-aXMIE1-BhppaLOcCI19PJY6lybkE0QhzkOE";
+
+
+  // Before each test load our todolistApp module
+  beforeEach(angular.mock.module('todolistApp'));
+
+  // Before each test set our injected Auth factory (_Auth_) to our local Auth variable
+  beforeEach(inject(function(_auth_, _$httpBackend_) {
+    auth = _auth_;
+    $httpBackend = _$httpBackend_;
+
+
+
+
+
+  }));
+
+  // A simple test to verify the Auth factory exists
+  it('should exist', function() {
+    expect(auth).toBeDefined();
+  });
+
+    // A set of tests for our Users.all() method
+    describe('.register()', function() {
+
+        it('should exist', function() {
+            expect(auth.register).toBeDefined();
+        });
+
     });
+
+    describe('.login()', function() {
+
+        it('should exist', function() {
+            expect(auth.logIn).toBeDefined();
+        });
+
+    });
+
+
 });
