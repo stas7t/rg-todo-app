81770046784d102976a30b47724f5a7e3534483b
stas7t <stas7t@gmail.com>
2017-02-24 13:38:58 +0200

    unit tests completed

--- public/angularApp/controllers/main.controller.test.js
+++ public/angularApp/controllers/main.controller.test.js
@@ -1,152 +1,3 @@
-/*
-    describe('MainCtrl', function() {
-    beforeEach(module('todolistApp'));
-
-    var $controller;
-
-    beforeEach(inject(function(_$controller_){
-        // The injector unwraps the underscores (_) from around the parameter names when matching
-        $controller = _$controller_;
-    }));
-
-    describe('$scope.grade', function() {
-
-        var $scope, controller;
-
-        beforeEach(function() {
-            $scope = {};
-
-
-
-            $scope.projects = [
-                {
-                    "id": 44,
-                    "name": "Test project 1",
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 45,
-                    "name": "Test project 2",
-                    "user_id": "5897ac559d925b00047096b4"
-                }
-                ];
-
-            $scope.tasks = [
-                {
-                    "id": 198,
-                    "name": "Task 2-1",
-                    "status": "uncompleted",
-                    "project_id": 45,
-                    "priority": 2,
-                    "deadline": null,
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 224,
-                    "name": "Task 1-4",
-                    "status": "uncompleted",
-                    "project_id": 44,
-                    "priority": 0,
-                    "deadline": null,
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 225,
-                    "name": "Task 1-5",
-                    "status": "completed",
-                    "project_id": 44,
-                    "priority": 0,
-                    "deadline": null,
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 221,
-                    "name": "Task 1-1",
-                    "status": "uncompleted",
-                    "project_id": 44,
-                    "priority": 7,
-                    "deadline": null,
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 222,
-                    "name": "Task 1-2",
-                    "status": "uncompleted",
-                    "project_id": 44,
-                    "priority": 5,
-                    "deadline": "2017-02-01T10:00:00.000Z",
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 223,
-                    "name": "Task 1-3",
-                    "status": "uncompleted",
-                    "project_id": 44,
-                    "priority": 0,
-                    "deadline": "2025-10-20T17:20:00.000Z",
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 217,
-                    "name": "Task 2-2",
-                    "status": "completed",
-                    "project_id": 45,
-                    "priority": 0,
-                    "deadline": "2018-02-18T12:20:00.000Z",
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 219,
-                    "name": "Task 2-4",
-                    "status": "completed",
-                    "project_id": 45,
-                    "priority": 0,
-                    "deadline": null,
-                    "user_id": "5897ac559d925b00047096b4"
-                },
-                {
-                    "id": 218,
-                    "name": "Task 2-3",
-                    "status": "uncompleted",
-                    "project_id": 45,
-                    "priority": 0,
-                    "deadline": "2018-02-17T12:30:00.000Z",
-                    "user_id": "5897ac559d925b00047096b4"
-                }
-            ];
-
-            
-            $scope.formDataProject = {name: "New test project"};
-            $scope.formDataTask = [{name: "New test project"}, {name: "New test project"}];
-
-            $scope.isLoggedIn = auth.isLoggedIn;
-
-            $scope.isPriorityChanged = false;
-
-            controller = $controller('MainCtrl', { $scope: $scope });
-        });
-
-        it('sets the strength to "strong" if the password length is >8 chars', function() {
-        $scope.password = 'longerthaneightchars';
-        $scope.grade();
-        expect($scope.strength).toEqual('strong');
-        });
-
-        it('sets the strength to "weak" if the password length <3 chars', function() {
-        $scope.password = 'a';
-        $scope.grade();
-        expect($scope.strength).toEqual('weak');
-        });
-
-    });
-    });
-*/
-
-
-
-
-
-
 describe('Main controller', function() {
     var $httpBackend;
     var $window;
@@ -195,6 +46,23 @@ describe('Main controller', function() {
             {   id: 2,
                 name: "Test task 002",
                 status: "uncompleted",
+                priority: 0,
+                deadline: null,
+                project_id: 1,
+                user_id: '58acac9054c6c20004592ae7'}
+    ];
+
+    var tasks1 = [
+            {   id: 11,
+                name: "Test task 011",
+                status: "uncompleted",
+                priority: 0,
+                deadline: null,
+                project_id: 1,
+                user_id: '58acac9054c6c20004592ae7'},
+            {   id: 12,
+                name: "Test task 012",
+                status: "uncompleted expired",
                 priority: 0,
                 deadline: null,
                 project_id: 1,
@@ -356,12 +224,13 @@ describe('Main controller', function() {
         $httpBackend.whenDELETE('/api/' + auth.currentUser()._id + '/projects/' + 2).respond(projectsAfterDel);
 
         $httpBackend.whenGET('/api/' + auth.currentUser()._id + '/tasks').respond(tasks);
-        $httpBackend.whenPOST('/api/' + auth.currentUser()._id + '/tasks').respond(tasksAfterAddNewOne);        //create new task
-        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  1).respond(tasksAfterRename);      //rename task #1
-        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  2).respond(tasksAfterComplete);    //complete task #2
-        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  3).respond(tasksAfterSetPriority); //set priority to task #3
-        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  4).respond(tasksAfterSetDeadline); //set deadline to task #4
-        $httpBackend.whenDELETE('/api/' + auth.currentUser()._id + '/tasks/' + 2).respond(tasksAfterDel);       // delete task #2
+        $httpBackend.whenPOST('/api/' + auth.currentUser()._id + '/tasks').respond(tasksAfterAddNewOne);         //create new task
+        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  1).respond(tasksAfterRename);       //rename task #1
+        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  2).respond(tasksAfterComplete);     //complete task #2
+        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  3).respond(tasksAfterSetPriority);  //set priority to task #3
+        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  4).respond(tasksAfterSetDeadline);  //set deadline to task #4
+        $httpBackend.whenPUT('/api/' + auth.currentUser()._id + '/tasks/' +  12).respond(tasks1); //for check deadline function
+        $httpBackend.whenDELETE('/api/' + auth.currentUser()._id + '/tasks/' + 2).respond(tasksAfterDel);        //delete task #2
 
     }));
 
@@ -776,21 +645,6 @@ describe('Main controller', function() {
         expect($scope.editedTask).toEqual(tasks[0]);
     });
 
-    it('should close any modal window'/*, function() {
-        var $scope = $rootScope.$new();
-
-        var controller = $controller('MainCtrl', { $scope: $scope });
-
-        $scope.editTaskModal = true;
-        $scope.tasks = tasks;
-
-        $scope.close_modal($scope.editTaskModal);
-
-        $httpBackend.flush();
-
-        expect($scope.editTaskModal).toEqual(false);
-    }*/);
-
     it('should set deadline for task #1', function() {
         var $scope = $rootScope.$new();
 
@@ -845,21 +699,20 @@ describe('Main controller', function() {
 
         var controller = $controller('MainCtrl', { $scope: $scope });
 
-        $scope.tasks = tasks;
+        $scope.tasks = tasks1;
 
         $scope.tasks[0].status = 'uncompleted';
         $scope.tasks[1].status = 'uncompleted';
 
-        $scope.tasks[1].deadline = '2019-03-01T10:00:00.000Z'; // future
-        $scope.tasks[0].deadline = '2015-03-01T10:00:00.000Z'; // expired
+        $scope.tasks[0].deadline = '2019-03-01T10:00:00.000Z'; // future
+        $scope.tasks[1].deadline = '2015-03-01T10:00:00.000Z'; // expired
 
         $scope.checkDeadline();
 
         $httpBackend.flush();
 
-
-        expect($scope.tasks[0].status).toEqual('uncompleted expired');
-        expect($scope.tasks[1].status).toEqual('completed');
+        expect($scope.tasks[0].status).toEqual('uncompleted');
+        expect($scope.tasks[1].status).toEqual('uncompleted expired');
     });    
 
 });
