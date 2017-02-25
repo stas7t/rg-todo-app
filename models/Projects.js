6ebe5864f9cd8dd47b4abffb3339f342a718c37f
stas7t <stas7t@gmail.com>
2017-02-26 01:32:54 +0200

    add `IF NOT EXISTS`

--- models/Projects.js
+++ models/Projects.js
@@ -4,5 +4,5 @@ let connectionString = process.env.DATABASE_URL || 'postgres://localhost';
 let client = new pg.Client(connectionString);
 
 client.connect();
-client.query('CREATE TABLE projects(id SERIAL PRIMARY KEY, name TEXT not null, user_id TEXT)');
+client.query('CREATE TABLE IF NOT EXISTS projects(id SERIAL PRIMARY KEY, name TEXT not null, user_id TEXT)');
 client.end();
--- models/Tasks.js
+++ models/Tasks.js
@@ -4,5 +4,5 @@ let connectionString = process.env.DATABASE_URL || 'postgres://localhost';
 let client = new pg.Client(connectionString);
 
 client.connect();
-client.query('CREATE TABLE tasks(id SERIAL PRIMARY KEY, name TEXT not null, status TEXT, priority SMALLINT, deadline TIMESTAMP WITH TIME ZONE, user_id TEXT, project_id INTEGER REFERENCES projects (id))');
+client.query('CREATE TABLE IF NOT EXISTS tasks(id SERIAL PRIMARY KEY, name TEXT not null, status TEXT, priority SMALLINT, deadline TIMESTAMP WITH TIME ZONE, user_id TEXT, project_id INTEGER REFERENCES projects (id))');
 client.end();
